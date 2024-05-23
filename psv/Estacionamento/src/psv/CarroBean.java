35b98c907700ea025875a9fcb1e16ea8c8fcc801
Fulano de Tal <giovanni.seloto.silva@gmail.com>
2024-05-22 22:22:38 -0300

    Adicionar arquivo da aula 22/05

---
+++ psv/Estacionamento/src/psv/CarroBean.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package psv;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class CarroBean {
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    private String placa; 
+    private String cor; 
+    private String descricao;
+}
+
+
